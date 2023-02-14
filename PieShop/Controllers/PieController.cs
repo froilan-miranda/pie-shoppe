0754f3c90e880e4e0273da07ac485d9a168738af
vaquero <froilan@virtual-artifact.com>
2023-02-14 21:35:55 +0100

    add views, models, and view models

---
+++ PieShop/Controllers/PieController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using PieShop.Models;
+using PieShop.ViewModels;
+
+namespace PieShop.Controllers;
+
+public class PieController : Controller
+{
+    private readonly IPieRepository _pieRepository;
+    private readonly ICategoryRepository _categoryRepository;
+
+    public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+    {
+        _pieRepository = pieRepository;
+        _categoryRepository = categoryRepository;
+    }
+    
+    public IActionResult List()
+    {
+        //ViewBag.Currentcategory = "Cheese cakes";
+        //return View(_pieRepository.AllPies);
+        PieListViewModel pieListViewModel = new PieListViewModel(_pieRepository.AllPies, "Cheese cakes");
+        return View(pieListViewModel);
+    }
+}
