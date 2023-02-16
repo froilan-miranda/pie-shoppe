fa447a5709fc408da2426ec1c4c46c0367ca4e63
vaquero <froilan@virtual-artifact.com>
2023-02-16 20:40:36 +0100

    add repositories, migrations and db context/connection

--- PieShop/Program.cs
+++ PieShop/Program.cs
@@ -1,10 +1,15 @@
+using Microsoft.EntityFrameworkCore;
 using PieShop.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
-builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IPieRepository, PieRepository>();
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<PieShopDbContext>(options =>
+{
+    options.UseSqlite(
+        builder.Configuration["ConnectionStrings:PieShopDbContextConnection"]);
+});
 var app = builder.Build();
 
 app.UseStaticFiles();
@@ -15,5 +20,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapDefaultControllerRoute();
-
+DbInitializer.Seed(app);
 app.Run();
